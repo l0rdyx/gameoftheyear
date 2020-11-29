f2540c0ec10b3ffb8fd58328939f4496df40bdad
l0rdyx <gleb.krasnopolinx@gmail.com>
2020-11-29 18:31:13 +0300

    fix

--- game/template.py
+++ game/template.py
@@ -3,24 +3,6 @@ import random
 from game import colors, params, player, tube, menue
 
 
-def game_intro():
-    intro = True
-
-    while intro:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        screen.fill(white)
-        largeText = pygame.font.Font('freesansbold.ttf', 115)
-        TextSurf, TextRect = text_objects("A bit Racey", largeText)
-        TextRect.center = ((display_width / 2), (display_height / 2))
-        gameDisplay.blit(TextSurf, TextRect)
-        pygame.display.update()
-        clock.tick(15)
-
 def run():
     pygame.init()
     pygame.mixer.init()
